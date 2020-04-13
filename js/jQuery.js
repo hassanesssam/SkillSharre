03c8044c42c5f64259bec260df3bbe527a636a41
Hassan Essam <63466256+hassanesssam@users.noreply.github.com>
2020-04-13 21:12:32 +0200

    My First website

---
+++ js/jQuery.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    $("#select").click(function(){
+        $("#hide").toggle();
+    });
+
+    $(".select-btn").click(function(){
+        $(".ul-hidden").fadeToggle(2500);
+
+    });
+
+    $(".select-btn1").click(function(){
+        $(".ul1-hidden").fadeToggle(2500);
+
+    });
+
+    $(".select-btn2").click(function(){
+        $(".ul2-hidden").fadeToggle(2500);
+
+    });
+
+    $(".select-btn3").click(function(){
+        $(".ul3-hidden").fadeToggle(2500);
+
+    });
+
+    $(".select-btn4").click(function(){
+        $(".ul4-hidden").fadeToggle(2500);
+
+    });
+
+    $(".select-btn5").click(function(){
+        $(".ul5-hidden").fadeToggle(2500);
+
+    });
+
+    $(".select-btn6").click(function(){
+        $(".ul6-hidden").fadeToggle(2500);
+
+    });
+
+    $(".select-btn7").click(function(){
+        $(".ul7-hidden").fadeToggle(2500);
+
+    });
+
+
+
+
+});
